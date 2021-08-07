86cc7dd0a710f67c866e8b77dac60a8ea72e6e05
michael.stevenson <michael.stevenson@finance-ni.gov.uk>
2021-08-07 17:08:50 +0100

    Added initial files

---
+++ edd-common-tools/Excel/ExportToExcel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace edd_common_tools.Excel
+{
+    class ExportToExcel
+    {
+    }
+}
---
+++ edd-common-tools/RequiredIf/RequiredIfAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace edd_common_tools.RequiredIf
+{
+    public class RequiredIfAttribute : ValidationAttribute
+    {
+        public string PropertyName { get; set; }
+        public object Value { get; set; }
+
+        public RequiredIfAttribute(string propertyName, object value, string errorMessage = "")
+        {
+            PropertyName = propertyName;
+            ErrorMessage = errorMessage;
+            Value = value;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var instance = validationContext.ObjectInstance;
+            var type = instance.GetType();
+            var proprtyvalue = type.GetProperty(PropertyName).GetValue(instance, null);
+            if (proprtyvalue.ToString() == Value.ToString() && value == null)
+            {
+                return new ValidationResult(ErrorMessage);
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
