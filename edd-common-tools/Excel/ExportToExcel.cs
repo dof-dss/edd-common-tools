4e6da9bf066b6d69eff23583f9cdeedeec1a0517
michael.stevenson <michael.stevenson@finance-ni.gov.uk>
2021-08-07 17:09:14 +0100

    code cleanup

--- edd-common-tools/Excel/ExportToExcel.cs
+++ edd-common-tools/Excel/ExportToExcel.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace edd_common_tools.Excel
+﻿namespace edd_common_tools.Excel
 {
-    class ExportToExcel
+    public class ExportToExcel
     {
     }
 }
